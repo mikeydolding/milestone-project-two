a82c5091f1c1370db7717827e62294156f591d2f
tekkoop <tekkoop@tutanota.com>
2023-05-04 00:38:21 +0300

    Changed fetch method

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,13 @@
 //        console.log(err);
 //    });
 
-//const searchFrom = document.querySelector('.search');
+const search = document.querySelector('.search');
 const input = document.querySelector('.input');
 const newsList = document.querySelector('.card-title');
-const search = document.getElementById('search');
+//const search = document.getElementById('search');
+
 const topic = 'entertainment';
+const apiKey = 'pub_21398e1dede5e8992ed2af8c2bc59bf9c8202';
 
 const newsType = document.getElementById("newsType");
 const newsTitle = document.getElementById("newsTitle");
@@ -25,33 +27,57 @@ const newsDescription = document.getElementById("newsDescription");
 
 var newsDataArr = [];
 
-search.addEventListener('click', function() {
-    newsType.innerHTML = "<h4>Search: " + topic + "<h4>";
-    fetchNews();
-});
+//search.addEventListener('click', function() {
+//    newsType.innerHTML = "<h4>Search: " + topic + "<h4>";
+//    fetchNews();
+//});
 
+search.addEventListener('submit', fetchNews);
 
-const fetchNews = async() => {
+//const fetchNews = async() => {
 
-    const apiKey = 'pub_21398e1dede5e8992ed2af8c2bc59bf9c8202';
+//    let url = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${topic}`;
+//    console.log('apiKey', apiKey)
+
+//    const response = await fetch(url);
+
+//    if (response.status >= 200 && response.status < 300) {
+//        const data = await response.json();
+//        newsDataArr = data.results;
+//        console.log('newsDataArr', newsDataArr)
+//    } else {
+//        //error handle
+//        console.log(response.status, response.statusText);
+//        newsdetails.innerHTML = "<h5>No data found.</h5>"
+//        return;
+//    }
+//    displayNews();
+//}
+
+function fetchNews(e) {
+    e.preventDefault();
     let url = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${topic}`;
-    console.log('apiKey', apiKey)
 
-    const response = await fetch(url);
-
-    if (response.status >= 200 && response.status < 300) {
-        const data = await response.json();
+    fetch(url).then((response) => {
+        return response.json()
+    }).then((data) => {
         newsDataArr = data.results;
         console.log('newsDataArr', newsDataArr)
+        displayNews();
 
-    } else {
-        //error handle
-        console.log(response.status, response.statusText);
-        newsdetails.innerHTML = "<h5>No data found.</h5>"
-        return;
-    }
-    displayNews();
+        //console.log(data)
+        //data.results.forEach(results => {
+        //    let li = document.createElement('li');
+        //    let a = document.createElement('a');
+        //    a.setAttribute('href', results.link);
+        //    a.setAttribute('target', '_blank');
+        //    a.textContent = results.title;
+        //    li.appendChild(a);
+        //    newsList.appendChild(li);
+        //});
+    }).catch((error) => { console.log(error) })
 }
+
 
 function displayNews() {
     newsdetails.innnerHTML = "";
@@ -89,25 +115,3 @@ function displayNews() {
 
     })
 }
-//function fetchNews(e) {
-//    e.preventDefault();
-
-//function fetchNews() {
-//    const apiKey = 'pub_21398e1dede5e8992ed2af8c2bc59bf9c8202 ';
-//    let url = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${topic}`;
-
-//    fetch(url).then((response) => {
-//        return response.json()
-//    }).then((data) => {
-//        console.log(data)
-//        data.results.forEach(results => {
-//            let li = document.createElement('li');
-//            let a = document.createElement('a');
-//            a.setAttribute('href', results.link);
-//            a.setAttribute('target', '_blank');
-//            a.textContent = results.title;
-//            li.appendChild(a);
-//            newsList.appendChild(li);
-//        });
-//    }).catch((error) => { console.log(error) })
-//}
